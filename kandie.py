8c261aa733f6c20827268982e5cc46cf3388126b
Mohadsk <115546857+Mohadsk@users.noreply.github.com>
2022-11-24 13:47:41 +0530

    Update kandie.py

--- kandie.py
+++ kandie.py
@@ -1,2 +1,5 @@
 Kandie is sweet.
-Kandie is delicious to eat
+Kandie is delicious to eat.
+It is a very good part of life to work with computers.
+My dream is to be an ethical hacker.
+Tata is the biggest brand in donating money.
